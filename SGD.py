d75cf2ac32492390e3be1b08b9a7b7404d51f6c3
Kawagoe <odaka.y.ac@m.titech.ac.jp>
2024-03-20 00:43:01 +0900

    SGD.pyを追加

---
+++ SGD.py
@@ -0,0 +1,45 @@
+# ライブラリのインポート
+import matplotlib.pyplot as plt
+import numpy as np
+import random
+import SGD_tools as tls
+import copy
+
+def SGD(w, grad, stepsize, lam, ch):
+    w = w - stepsize * grad
+    if ch == 1:
+        w = tls.prox1(w, lam)
+    elif ch == 2:
+        w = tls.prox2(w, lam)
+    return w
+
+n = 10000
+d = 50
+data = tls.dataset(n, d)
+stepsize = 1 / (2*data.Lip)
+lam = 1e-6
+
+# 初期値をすべて1のベクトルを取る
+w = np.array([1]*d, dtype="float")
+# 最適点と初期値との距離を計算する.あとで使う.
+dist0 = np.linalg.norm(data.opt - w)
+# dis0と現在の最適点との距離をリストにする.
+dist_his = [1]
+for i in range(20*n):
+    # 勾配に用いるサンプルを取る
+    j = np.random.randint(n)
+    x = data.X[j]
+    y = data.Y[j]
+    fj = tls.func(w)
+    # wを更新
+    w = SGD(w, fj.loss_grad(x, y), stepsize, lam, 0)
+    # 距離を求めてdist_hisを更新
+    dist = np.linalg.norm(data.opt - w)
+    dist_his.append(dist/dist0)
+    print(i, tls.dig_cut(dist/dist0, 8), end="\r")
+
+# グラフを描画
+plt.yscale('log')
+plt.minorticks_on()
+plt.plot(dist_his)
+plt.show()
--- SGD_tools.py
+++ SGD_tools.py
@@ -3,64 +3,93 @@ import numpy as np
 import random
 from scipy.sparse.linalg import eigs
 
-def func(f, x):
-    return np.dot(f, x)
+#一般の線形回帰モデルを考える
+class func:
+    def __init__(self, A:list[float]):
+        #A：係数リスト（index0の値は定数項）
+        #dig：入力データxの次元の数
+        self.coef = np.array(A)
+        self.dig = len(A)
+    
+    def get(self, x:list[float]) -> float: #f(x)の値を取得する
+        return np.dot(self.coef, x)
 
-def loss(a, x, y):
-    return (func(a, x) - y)**2
+    def SE(self, x:list[float], y:float) -> float: #二乗誤差を計算
+        return (self.get(x) - y)**2
 
-def loss_mean(a, X, Y):
-    val = 0
-    n = len(X)
+    def MSE(self, x:list[list[float]], y:list[float]) -> float: #平均二乗誤差を計算
+        res = 0
+        n = len(y)
+        for i in range(n):
+            res += self.SE(x[i], y[i])
+        return res / n
+
+    def loss_grad(self, x:list[float], y:float) -> list[float]: #損失関数の勾配を計算
+        n = len(x)
+        res = [None] * n
+        temp = self.get(x) - y
+        for i in range(n):
+            res[i] = 2 * x[i] * temp
+        return np.array(res)
+
+#係数リストAに対してf(x) = A[0] + A[1]x +A[2]x^2 + A[3]x^3 + ...の場合（多項式関数）
+class poly_func(func):
+    def __init__(self, A:list[float]):
+        super().__init__(A)
+
+    def get_poly(self, x:float) -> float:
+        n = self.dig
+        temp = [None]*n
+        for i in range(n):
+            temp[i] = x**i
+        return super().get(temp)
+
+    def SE_poly(self, x:float, y:float) -> float:
+        return (self.get_poly(x) - y)**2
+
+    def MSE_poly(self, x:list[float], y:list[float]) -> float:
+        res = self.coef[0]
+        n = len(y)
+        for i in range(n):
+            res += self.SE_poly(x[i], y[i])
+        return res / n
+
+    def loss_grad_poly(self, x:float, y:float) -> list[float]: #損失関数の勾配を計算
+        n = self.dig
+        temp = [None]*n
+        for i in range(n):
+            temp[i] = x**i
+        return super().loss_grad(temp, y)
+
+#実験に使うデータセットを生成
+class dataset:
+    def __init__(self, n:int, d:int):
+        self.opt = np.random.randn(d) #幅を持った値で試すと良し
+        self.X = np.random.normal(0, 5, (n, d))
+        temp = []
+        temp_f = func(self.opt)
+        for x in self.X:
+            temp.append(temp_f.get(x) + np.random.randn())
+        self.Y = np.array(temp)
+        A = np.matmul((self.X).T, self.X)
+        self.Lip = 2 * eigs(A, 1)[0].real
+
+#L1正則化(定数g)
+def prox1(w:list[float], g:float) -> list[float]:
+    n = len(w)
+    new_w = [None]*n
     for i in range(n):
-        val += loss(a, X[i], Y[i]) / n
-    return val
+        temp = w[i]
+        if temp > g:
+            new_w[i] = temp - g
+        elif temp < -g:
+            new_w[i] = temp + g
+    return new_w
 
-def loss_grad(a, x, y):
-    return 2 * x * (func(a, x) - y)
+#L2正則化(定数g)
+def prox2(w:list[float], g:float) -> list[float]:
+    return w * (1/(1+g))
 
-def loss_var(w, X, Y):
-    n = len(Y)
-    var = 0
-    mean = 0
-    for i in range(n):
-        mean += loss(w, X[i], Y[i]) / n
-    for i in range(n):
-        var += (loss(w, X[i], Y[i]) - mean)**2 / n
-    return var
-
-def prox(a, gam):
-    n = len(a)
-    new_a = [0]*n
-    for i in range(n):
-        if a[i] > gam:
-            new_a[i] = a[i] - gam
-        elif a[i] < -gam:
-            new_a[i] = a[i] + gam
-    return new_a
-
-def prox2(a, gam):
-    return np.array(a) * (1/(1+gam))
-
-def dataset_aaa(n, d): # 多分もう使わない
-    X = np.random.randn(n, d)
-    Y = np.array([1]*n, dtype = "float")
-    A = np.matmul(X.T, X)
-    B = np.matmul(X.T, Y)
-    W = np.matmul(np.linalg.pinv(A), B)
-    return X, Y, W
-
-def dataset(n, d):
-    w = np.random.randn(d) #幅を持った値で試すと良し
-    X = np.random.normal(0, 5, (n, d))
-    Y = []
-    for x in X:
-        Y.append(func(w, x) + np.random.randn())
-    return X, Y, w
-
-def Lip_const(X):
-    A = np.matmul(X.T, X)
-    return 2 * eigs(A, 1)[0].real
-
-def dig_cut(val, dig):
-    return int(val*10**dig)/10**dig
+###出力を見やすくする関数(d桁まで表示)
+def dig_cut(val, d):
+    return int(val*10**d)/10**d
